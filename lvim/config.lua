04ab37983923d78fa628c80d1db93b2410040da7
Anzar Malik <12586091+Mediocorite@users.noreply.github.com>
2024-12-25 11:49:35 +0000

    Initial commit

---
+++ lvim/config.lua
@@ -0,0 +1,29 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+-- Glogbal keybindings
+vim.opt.relativenumber = true;
+
+-- Autocomplete modifications
+local cmp = require('cmp')
+lvim.builtin.cmp.mapping["<CR>"] = cmp.mapping.confirm({ select = true })
+
+-- Copilot
+lvim.plugins = {
+  { "github/copilot.vim" },
+  { "hrsh7th/cmp-copilot" }
+}
+
+
+-- Configure Copilot
+
+-- Add Copilot as a completion source
+table.insert(lvim.builtin.cmp.sources, { name = "copilot" })
+
+-- Customize the display name in completion menu
+lvim.builtin.cmp.formatting.source_names["copilot"] = "(Cop)"
+
+-- Fixing the shift arrow behavior
